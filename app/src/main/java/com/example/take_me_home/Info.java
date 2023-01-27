5003c337da4240fa49094bddcd681ac2387ba806
ranganmukherjee <justlikethatrangan@gmail.com>
2023-01-27 15:35:24 +0530

    version 2.5.1

---
+++ app/src/main/java/com/example/take_me_home/Info.java
@@ -0,0 +1,10 @@
+package com.example.take_me_home;
+
+public class Info {
+    static String name;
+    static  String email;
+    static String prof;
+    static String cno;
+    static String dlink;
+
+}
